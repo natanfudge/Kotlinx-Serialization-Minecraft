512b3068eed084022ee468f9b04e2ced6fd99923
natanfudge <natan.lifsiz@gmail.com>
2019-08-10 15:07:36 +0300

    God rid of minecraft stuff

--- src/main/kotlin/drawer/TagEncoder.kt
+++ src/main/kotlin/drawer/TagEncoder.kt
@@ -1,76 +1,76 @@
-//import kotlinx.serialization.*
-//import kotlinx.serialization.internal.EnumDescriptor
-//import net.minecraft.nbt.CompoundTag
-//
-////TODO: document usage:
-//// Badges: discord, bintray, curseforge
-////TODO: more formats: TextComponent (steal https://github.com/Juuxel/Adorn/blob/1f979868c1744bfa80d6bf40c4ac5f6c265f3d0f/src/main/kotlin/juuxel/adorn/block/entity/TradingStationBlockEntity.kt),
-//// ItemStack, DefaultedList,
-////TODO: test nullable data class
-////TODO: test readme
-//
-//
-///**
-// * This demo shows another approach to serialization:
-// * Instead of writing fields and their values separately in two steps, as ElementValueOutput does,
-// * in NamedValueOutput they got merged into one call.
-// *
-// * NamedValue is a subclass of TaggedValue, which allows you to associate any custom tag with object's field,
-// * see TaggedDemo.kt.
-// *
-// * Here, the tag is field's name. Functionality of these classes is similar to kotlinx.serialization.Mapper
-// * Note that null values are not supported here.
-// */
-//
-//internal class TagEncoder(val compoundTag: CompoundTag = CompoundTag()) : NamedValueEncoder() {
-//    override fun encodeTaggedBoolean(tag: String, value: Boolean) = compoundTag.putBoolean(tag, value)
-//    override fun beginCollection(desc: SerialDescriptor, collectionSize: Int, vararg typeParams: KSerializer<*>):
-//            CompositeEncoder {
-//        encodeTaggedInt(nested("size"), collectionSize)
-//        return this
-//    }
-//
-//    override fun encodeTaggedByte(tag: String, value: Byte) = compoundTag.putByte(tag, value)
-//    override fun encodeTaggedChar(tag: String, value: Char) = compoundTag.putString(tag, value.toString())
-//    override fun encodeTaggedDouble(tag: String, value: Double) = compoundTag.putDouble(tag, value)
-//    override fun encodeTaggedEnum(tag: String, enumDescription: EnumDescriptor, ordinal: Int) =
-//        compoundTag.putInt(tag, ordinal)
-//
-//    override fun encodeTaggedFloat(tag: String, value: Float) = compoundTag.putFloat(tag, value)
-//    override fun encodeTaggedInt(tag: String, value: Int) = compoundTag.putInt(tag, value)
-//    override fun encodeTaggedLong(tag: String, value: Long) = compoundTag.putLong(tag, value)
-//    override fun encodeTaggedNotNullMark(tag: String) = compoundTag.putByte(tag + "mark", 1)
-//    override fun encodeTaggedNull(tag: String) {
-//        compoundTag.putByte(tag + "mark", 0)
-//    }
-//
-//    override fun encodeTaggedShort(tag: String, value: Short) = compoundTag.putShort(tag, value)
-//    override fun encodeTaggedString(tag: String, value: String) = compoundTag.putString(tag, value)
-//    override fun encodeTaggedUnit(tag: String) = compoundTag.putByte(tag, 2)
-//
-//}
-//
-//internal class TagDecoder(private val map: CompoundTag) : NamedValueDecoder() {
-//    override fun decodeCollectionSize(desc: SerialDescriptor): Int {
-//        return decodeTaggedInt(nested("size"))
-//    }
-//
-//    override fun decodeTaggedBoolean(tag: String) = map.getBoolean(tag)
-//    override fun decodeTaggedByte(tag: String) = map.getByte(tag)
-//    override fun decodeTaggedChar(tag: String) = map.getString(tag).toCharArray()[0]
-//    override fun decodeTaggedDouble(tag: String) = map.getDouble(tag)
-//    override fun decodeTaggedEnum(tag: String, enumDescription: EnumDescriptor) =
-//        map.getInt(tag)
-//
-//    override fun decodeTaggedFloat(tag: String) = map.getFloat(tag)
-//    override fun decodeTaggedInt(tag: String) = map.getInt(tag)
-//    override fun decodeTaggedLong(tag: String) = map.getLong(tag)
-//    override fun decodeTaggedNotNullMark(tag: String) = map.getByte(tag + "mark") != 0.toByte()
-//    override fun decodeTaggedShort(tag: String) = map.getShort(tag)
-//
-//    override fun decodeTaggedString(tag: String): String = map.getString(tag)
-//    override fun decodeTaggedUnit(tag: String) = Unit
-//
-//}
-//
-//
+import kotlinx.serialization.*
+import kotlinx.serialization.internal.EnumDescriptor
+import net.minecraft.nbt.CompoundTag
+
+//TODO: document usage:
+// Badges: discord, bintray, curseforge
+//TODO: more formats: TextComponent (steal https://github.com/Juuxel/Adorn/blob/1f979868c1744bfa80d6bf40c4ac5f6c265f3d0f/src/main/kotlin/juuxel/adorn/block/entity/TradingStationBlockEntity.kt),
+// ItemStack, DefaultedList,
+//TODO: test nullable data class
+//TODO: test readme
+
+
+/**
+ * This demo shows another approach to serialization:
+ * Instead of writing fields and their values separately in two steps, as ElementValueOutput does,
+ * in NamedValueOutput they got merged into one call.
+ *
+ * NamedValue is a subclass of TaggedValue, which allows you to associate any custom tag with object's field,
+ * see TaggedDemo.kt.
+ *
+ * Here, the tag is field's name. Functionality of these classes is similar to kotlinx.serialization.Mapper
+ * Note that null values are not supported here.
+ */
+
+internal class TagEncoder(val compoundTag: CompoundTag = CompoundTag()) : NamedValueEncoder() {
+    override fun encodeTaggedBoolean(tag: String, value: Boolean) = compoundTag.putBoolean(tag, value)
+    override fun beginCollection(desc: SerialDescriptor, collectionSize: Int, vararg typeParams: KSerializer<*>):
+            CompositeEncoder {
+        encodeTaggedInt(nested("size"), collectionSize)
+        return this
+    }
+
+    override fun encodeTaggedByte(tag: String, value: Byte) = compoundTag.putByte(tag, value)
+    override fun encodeTaggedChar(tag: String, value: Char) = compoundTag.putString(tag, value.toString())
+    override fun encodeTaggedDouble(tag: String, value: Double) = compoundTag.putDouble(tag, value)
+    override fun encodeTaggedEnum(tag: String, enumDescription: EnumDescriptor, ordinal: Int) =
+        compoundTag.putInt(tag, ordinal)
+
+    override fun encodeTaggedFloat(tag: String, value: Float) = compoundTag.putFloat(tag, value)
+    override fun encodeTaggedInt(tag: String, value: Int) = compoundTag.putInt(tag, value)
+    override fun encodeTaggedLong(tag: String, value: Long) = compoundTag.putLong(tag, value)
+    override fun encodeTaggedNotNullMark(tag: String) = compoundTag.putByte(tag + "mark", 1)
+    override fun encodeTaggedNull(tag: String) {
+        compoundTag.putByte(tag + "mark", 0)
+    }
+
+    override fun encodeTaggedShort(tag: String, value: Short) = compoundTag.putShort(tag, value)
+    override fun encodeTaggedString(tag: String, value: String) = compoundTag.putString(tag, value)
+    override fun encodeTaggedUnit(tag: String) = compoundTag.putByte(tag, 2)
+
+}
+
+internal class TagDecoder(private val map: CompoundTag) : NamedValueDecoder() {
+    override fun decodeCollectionSize(desc: SerialDescriptor): Int {
+        return decodeTaggedInt(nested("size"))
+    }
+
+    override fun decodeTaggedBoolean(tag: String) = map.getBoolean(tag)
+    override fun decodeTaggedByte(tag: String) = map.getByte(tag)
+    override fun decodeTaggedChar(tag: String) = map.getString(tag).toCharArray()[0]
+    override fun decodeTaggedDouble(tag: String) = map.getDouble(tag)
+    override fun decodeTaggedEnum(tag: String, enumDescription: EnumDescriptor) =
+        map.getInt(tag)
+
+    override fun decodeTaggedFloat(tag: String) = map.getFloat(tag)
+    override fun decodeTaggedInt(tag: String) = map.getInt(tag)
+    override fun decodeTaggedLong(tag: String) = map.getLong(tag)
+    override fun decodeTaggedNotNullMark(tag: String) = map.getByte(tag + "mark") != 0.toByte()
+    override fun decodeTaggedShort(tag: String) = map.getShort(tag)
+
+    override fun decodeTaggedString(tag: String): String = map.getString(tag)
+    override fun decodeTaggedUnit(tag: String) = Unit
+
+}
+
+
--- src/main/kotlin/drawer/mc/Drawer.kt
+++ src/main/kotlin/drawer/mc/Drawer.kt
@@ -1,80 +1,6 @@
 //package drawer.mc
 //
-//import drawer.convertToTag
-//import drawer.mc.util.Builders
-//import drawer.mc.util.DefaultBlock
-//import drawer.mc.util.ModInit
-//import kotlinx.serialization.KSerializer
-//import kotlinx.serialization.serializer
-//import net.minecraft.block.BlockEntityProvider
-//import net.minecraft.block.BlockState
-//import net.minecraft.block.entity.BlockEntity
-//import net.minecraft.block.entity.BlockEntityType
-//import net.minecraft.entity.player.PlayerEntity
-//import net.minecraft.nbt.CompoundTag
-//import net.minecraft.util.Hand
-//import net.minecraft.util.hit.BlockHitResult
-//import net.minecraft.util.math.BlockPos
-//import net.minecraft.util.registry.Registry
-//import net.minecraft.world.BlockView
-//import net.minecraft.world.World
-//import kotlin.reflect.KProperty
-//
-//class Bar<T : Any?>(val value: T)
-//
-//fun <T : Any?> foo(x: T) {
-//
-//}
-//
-//fun main() {
-//    val x = Bar(1)
-//    foo(x.value)
-//}
-//
-//
-////TODO: key
-//data class MemorizedValue<T>(val value: T, val serializer: KSerializer<T>)
-//
-//abstract class AutoSerializable(type: BlockEntityType<*>) : BlockEntity(type) {
-//    val values = mutableMapOf<String, Any?>()
-//    private val serializers = mutableMapOf<String, KSerializer<Any?>>()
-//
-//    override fun toTag(tag: CompoundTag): CompoundTag {
-//        val data = CompoundTag().apply {
-//            for ((k, v) in values) {
-//                put(k, serializers.getValue(k).convertToTag(v))
-//            }
-//        }
-//
-//        tag.put("key", data)
-//
-//        return super.toTag(tag)
-//    }
-//
-//    override fun fromTag(tag: CompoundTag) {
-//        super.fromTag(tag)
-//        val data = tag.getTag("key") as CompoundTag
-//        for (key in data.keys){
-//            values.put(key,data.)
-//        }
-//    }
-//
-//    operator fun < T> KSerializer<T>.getValue(thisRef: Any?, property: KProperty<*>): T? {
-//        return values[property.name] as? T
-//    }
-//
-//    operator fun <T> KSerializer<T>.setValue(thisRef: Any?, property: KProperty<*>, value: T?): T? {
-//        values[property.name] = value
-//        serializers[property.name] = this as KSerializer<Any?>
-//        return value
-//    }
-//
-//}
-//
-//
-//fun SerializerDelegate() {
-//
-//}
+
 //
 //const val ModId = "fabricdrawer"
 //
@@ -91,40 +17,14 @@
 //
 //val BlockEntityType = Builders.blockEntityType(TestBlock) { TestBlockEntity() }
 //
-//class TestBlockEntity : AutoSerializable(BlockEntityType) {
-//    var num: Int? by Int.serializer()
-////    override fun toTag(compoundTag_1: CompoundTag?): CompoundTag {
-////        return super.toTag(compoundTag_1)
-////    }
-////
-////    override fun fromTag(compoundTag_1: CompoundTag?) {
-////        super.fromTag(compoundTag_1)
-////    }
+//class TestBlockEntity : BlockEntity(BlockEntityType) {
+
 //}
 //
-////private operator fun <T> KSerializer<T>.setValue(testBlockEntity: TestBlockEntity, property: KProperty<*>, t: T?) {
-////        TODO("not implemented")
-////}
-////
-////private operator fun <T> KSerializer<T>.getValue(testBlockEntity: TestBlockEntity, property: KProperty<*>): T {
-////    TODO("not implemented")
-////}
+
 //
 //object TestBlock : DefaultBlock(), BlockEntityProvider {
 //    override fun createBlockEntity(var1: BlockView): BlockEntity = TestBlockEntity()
-//
-//
-//    override fun activate(
-//        blockState_1: BlockState?,
-//        world: World,
-//        pos: BlockPos,
-//        playerEntity_1: PlayerEntity?,
-//        hand_1: Hand?,
-//        blockHitResult_1: BlockHitResult?
-//    ): Boolean {
-//        (world.getBlockEntity(pos) as TestBlockEntity).num++
-//        return super.activate(blockState_1, world, pos, playerEntity_1, hand_1, blockHitResult_1)
-//    }
-//
+
 //
 //}
